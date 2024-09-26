a6cd35e69db7de5b38846a8894495e03ef47c7e1
cmbbigis <g0r1lla.disk@gmail.com>
2024-09-26 11:50:54 +0500

    hw1: nonUniqueElements done

--- nonUniqueElements.js
+++ nonUniqueElements.js
@@ -25,6 +25,5 @@ nonUniqueElements([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
  */
 
 export default function nonUniqueElements(data) {
-  // your solution goes here
-  return data
+  return data.filter((item, index, self) => self.indexOf(item) !== self.lastIndexOf(item))
 }
