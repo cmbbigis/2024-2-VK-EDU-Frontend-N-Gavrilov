14b6e89f56f91c43e5c4ba5a7c4b4d5125324a21
cmbbigis <cmbbigis@users.noreply.github.com>
2024-10-03 10:37:31 +0000

    deploy: a96a52850e2f8151efd02077fee26e80e823188c

---
+++ bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var form = document.querySelector('form');\nvar messageInput = document.querySelector('.message-input');\nvar nameInput = document.querySelector('.name-input');\nvar messagesContainer = document.querySelector('.messages');\nform.addEventListener('submit', handleSubmit);\nform.addEventListener('keypress', handleKeyPress);\nloadMessages();\nfunction handleSubmit(event) {\n  event.preventDefault();\n  var text = messageInput.value.trim();\n  var sender = nameInput.value.trim() || 'Anonymous';\n  if (text) {\n    saveMessage(text, sender);\n    loadMessages();\n    messageInput.value = '';\n    nameInput.value = '';\n  }\n}\nfunction handleKeyPress(event) {\n  if (event.keyCode === 13) {\n    form.dispatchEvent(new Event('submit'));\n  }\n}\nfunction loadMessages() {\n  var messages = JSON.parse(localStorage.getItem('messages')) || [];\n  messagesContainer.innerHTML = '';\n  messages.forEach(function (_ref) {\n    var text = _ref.text,\n      sender = _ref.sender,\n      time = _ref.time;\n    var messageDiv = document.createElement('div');\n    messageDiv.classList.add('message');\n    messageDiv.innerHTML = \"<strong>\".concat(sender, \"</strong> <em>\").concat(time, \"</em><br>\").concat(text);\n    messagesContainer.appendChild(messageDiv);\n  });\n}\nfunction saveMessage(text, sender) {\n  var messages = JSON.parse(localStorage.getItem('messages')) || [];\n  var time = new Date().toLocaleString();\n  messages.push({\n    text: text,\n    sender: sender,\n    time: time\n  });\n  localStorage.setItem('messages', JSON.stringify(messages));\n}\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
