53501a86528fe19a0f3762d3f1c2aba0f1b74ad7
cmbbigis <g0r1lla.disk@gmail.com>
2024-10-03 14:14:59 +0500

    hw2: скрипты переехали в index.js

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -1,1 +1,17 @@
-import './index.css';
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+const message = document.querySelector('.message');
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keypress', handleKeyPress);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    message.innerText = input.value;
+}
+
+function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+        form.dispatchEvent(new Event('submit'));
+    }
+}
