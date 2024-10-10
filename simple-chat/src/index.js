0b9a898d4127cc0f9a0c7b2a74ff343eda76d870
cmbbigis <g0r1lla.disk@gmail.com>
2024-10-10 12:48:11 +0500

    hw2: правки после ревью

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -2,29 +2,20 @@ import './index.css';
 
 const form = document.querySelector('form');
 const messageInput = document.querySelector('.message-input');
-const nameInput = document.querySelector('.name-input');
 const messagesContainer = document.querySelector('.messages');
 
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('keypress', handleKeyPress);
 
 loadMessages();
 
 function handleSubmit(event) {
     event.preventDefault();
     const text = messageInput.value.trim();
-    const sender = nameInput.value.trim() || 'Anonymous';
+    const sender = 'Я';
     if (text) {
         saveMessage(text, sender);
         loadMessages();
         messageInput.value = '';
-        nameInput.value = '';
-    }
-}
-
-function handleKeyPress(event) {
-    if (event.keyCode === 13) {
-        form.dispatchEvent(new Event('submit'));
     }
 }
 
@@ -34,7 +25,7 @@ function loadMessages() {
     messages.forEach(({ text, sender, time }) => {
         const messageDiv = document.createElement('div');
         messageDiv.classList.add('message');
-        messageDiv.innerHTML = `<strong>${sender}</strong> <em>${time}</em><br>${text}`;
+        messageDiv.innerHTML = `<strong>${sender}</strong> (<em>${time}</em>):<br>${text}`;
         messagesContainer.appendChild(messageDiv);
     });
 }
