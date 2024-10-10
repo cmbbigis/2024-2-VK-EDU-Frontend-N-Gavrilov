ab98c3eafdaba5802281c28f8039c9f7dd3a4df9
cmbbigis <cmbbigis@users.noreply.github.com>
2024-10-10 14:37:36 +0000

    deploy: 25de6f7454a080b60b4eaf2c9e1707a38f2021a2

--- chatList.bundle.js
+++ chatList.bundle.js
@@ -94,7 +94,31 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_chat_list_chatListHeader_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/chat-list/chatListHeader.js */ \"./components/chat-list/chatListHeader.js\");\n\ndocument.addEventListener('DOMContentLoaded', function () {\n  var app = document.getElementById('app');\n  var chatListHeader = Object(_components_chat_list_chatListHeader_js__WEBPACK_IMPORTED_MODULE_0__[\"createChatListHeader\"])();\n  app.appendChild(chatListHeader);\n});\n\n//# sourceURL=webpack:///./chat-list/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_chat_list_chatListHeader_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/chat-list/chatListHeader.js */ \"./components/chat-list/chatListHeader.js\");\n/* harmony import */ var _components_chat_list_chatList_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/chat-list/chatList.js */ \"./components/chat-list/chatList.js\");\n/* harmony import */ var _components_chat_list_chat_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/chat-list/chat.js */ \"./components/chat-list/chat.js\");\n\n\n\ndocument.addEventListener('DOMContentLoaded', function () {\n  var app = document.getElementById('app');\n  var chatListHeader = Object(_components_chat_list_chatListHeader_js__WEBPACK_IMPORTED_MODULE_0__[\"createChatListHeader\"])();\n  var chatList = Object(_components_chat_list_chatList_js__WEBPACK_IMPORTED_MODULE_1__[\"createChatList\"])();\n  var chat = Object(_components_chat_list_chat_js__WEBPACK_IMPORTED_MODULE_2__[\"createChat\"])();\n  chatList.appendChild(chat);\n  app.appendChild(chatListHeader);\n  app.appendChild(chatList);\n});\n\n//# sourceURL=webpack:///./chat-list/index.js?");
+
+/***/ }),
+
+/***/ "./components/chat-list/chat.js":
+/*!**************************************!*\
+  !*** ./components/chat-list/chat.js ***!
+  \**************************************/
+/*! exports provided: createChat */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"createChat\", function() { return createChat; });\nfunction createChat() {\n  var chat = document.createElement('div');\n  chat.className = 'chat';\n  var avatar = document.createElement('img');\n  avatar.className = 'avatar';\n  avatar.alt = 'Avatar';\n  avatar.src = \"../chat/pictures/avatar.png\";\n  var chatInfo = document.createElement('div');\n  chatInfo.className = 'chat-info';\n  var chatTitle = document.createElement('h2');\n  chatTitle.className = 'chat-title';\n  chatTitle.innerText = 'Дженнифер Эшли';\n  var chatLastMessage = document.createElement('p');\n  chatLastMessage.className = 'chat-last-message';\n  chatLastMessage.innerText = 'Ты куда пропал?';\n  var chatMeta = document.createElement('div');\n  chatMeta.className = 'chat-meta';\n  var lastMessageTime = document.createElement('span');\n  lastMessageTime.className = 'last-message-time';\n  lastMessageTime.innerText = '15:52';\n  var status = document.createElement('span');\n  status.className = 'material-symbols-outlined';\n  status.innerText = 'done_all';\n  chatInfo.appendChild(chatTitle);\n  chatInfo.appendChild(chatLastMessage);\n  chatMeta.appendChild(lastMessageTime);\n  chatMeta.appendChild(status);\n  chat.appendChild(avatar);\n  chat.appendChild(chatInfo);\n  chat.appendChild(chatMeta);\n  return chat;\n}\n\n//# sourceURL=webpack:///./components/chat-list/chat.js?");
+
+/***/ }),
+
+/***/ "./components/chat-list/chatList.js":
+/*!******************************************!*\
+  !*** ./components/chat-list/chatList.js ***!
+  \******************************************/
+/*! exports provided: createChatList */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"createChatList\", function() { return createChatList; });\nfunction createChatList() {\n  var chatList = document.createElement('div');\n  chatList.className = 'chat-list';\n  return chatList;\n}\n\n//# sourceURL=webpack:///./components/chat-list/chatList.js?");
 
 /***/ }),
 
