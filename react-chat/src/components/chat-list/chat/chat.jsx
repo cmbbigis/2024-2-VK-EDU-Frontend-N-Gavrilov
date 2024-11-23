7b4de44ca0e515b903c3c083cf1a08f446a8edb9
cmbbigis <g0r1lla.disk@gmail.com>
2024-11-23 21:20:36 +0500

    hw7: костыльно научился грузить чаты с бэка

--- react-chat/src/components/chat-list/chat/chat.jsx
+++ react-chat/src/components/chat-list/chat/chat.jsx
@@ -4,18 +4,18 @@ import DoneAllIcon from '@mui/icons-material/DoneAll';
 
 import './chat.scss';
 
-export const Chat = ({ id, interlocutor, avatar }) => {
-    const lastMessage = getLastMessage(id);
+export const Chat = ({ id, interlocutor, avatar, lastMessage }) => {
+    const lastMessageInfo = getLastMessage(lastMessage);
     return (
         <Link to={`chat/${id}`} key={id} className={"chat-link"}>
             <div className="chat">
                 <img className="avatar" alt="Avatar" src={avatar} />
                 <div className="chat-info">
                     <h2 className="chat-title">{interlocutor}</h2>
-                    <p className="chat-last-message">{lastMessage.text}</p>
+                    <p className="chat-last-message">{lastMessageInfo.text}</p>
                 </div>
                 <div className="chat-meta">
-                    <span className="last-message-time">{lastMessage.time}</span>
+                    <span className="last-message-time">{""}</span>
                     <DoneAllIcon />
                 </div>
             </div>
@@ -23,8 +23,13 @@ export const Chat = ({ id, interlocutor, avatar }) => {
     );
 }
 
-function getLastMessage(chatId) {
-    const messages = JSON.parse(localStorage.getItem('messages')) || [];
-    const filteredMessages = messages.filter(message => message.chatId === chatId) || [];
-    return filteredMessages[filteredMessages.length - 1] || { text: '', time: '' };
+function getLastMessage(messageId) {
+    if (messageId === null || messageId === undefined) {
+        return { text: '', time: '' };
+    } else {
+        return messageId;
+    }
+    // const messages = JSON.parse(localStorage.getItem('messages')) || [];
+    // const filteredMessages = messages.filter(message => message.chatId === messageId) || [];
+    // return filteredMessages[filteredMessages.length - 1] || { text: '', time: '' };
 }
--- react-chat/src/components/chat-list/chatList/chatList.jsx
+++ react-chat/src/components/chat-list/chatList/chatList.jsx
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 import './chatList.scss';
 import { Chat } from "../chat";
+import {BackendHttpClient} from "../../../utils/backendHttpClient";
 
 export const ChatList = ({ reload }) => {
     const [chats, setChats] = useState([]);
 
     useEffect(() => {
-        setChats(loadChats());
+        loadChats().then(r => {
+            return r;
+        });
     }, [reload]);
 
-    function loadChats() {
-        return JSON.parse(localStorage.getItem('chats')) || [];
+    async function loadChats() {
+        setChats((await BackendHttpClient.getChats(null, null, null))["results"]);
     }
 
     return (
         <div id="chat-list" className="chat-list">
-            {chats.map(({id, interlocutor, avatar}) => (
-                <Chat id={id} interlocutor={interlocutor} avatar={avatar}/>
+            {chats.map((chat) => (
+                <Chat key={chat["id"]} id={chat["id"]} interlocutor={chat["title"]} avatar={chat["avatar"]} lastMessage={chat["last_message"]}/>
             ))}
         </div>
     );
