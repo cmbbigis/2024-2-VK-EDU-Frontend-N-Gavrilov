ed2d4e1257e2d18aa6015abda7ae56cf5dca4e9f
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-23 22:56:59 +0500

    exam: переход на страницу истории

--- exam/src/pages/translate/TranslatePage.tsx
+++ exam/src/pages/translate/TranslatePage.tsx
@@ -2,11 +2,16 @@ import {Header} from "../../components/Header/Header.tsx";
 import {TranslateWindow} from "../../components/TranslateWindow/TranslateWindow.tsx";
 
 import './TranslatePage.css';
+import {Link} from "react-router-dom";
+import RestoreIcon from '@mui/icons-material/Restore';
 
 export const TranslatePage = () => {
     return (
         <div className='translate-page'>
             <Header title="VK Translate" />
+            <Link to={"/history/"} className="history-link">
+                <RestoreIcon />
+            </Link>
             <TranslateWindow />
         </div>
     );
