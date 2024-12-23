c54bb7a6935852a81112db959d674196a183e028
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-23 20:46:05 +0500

    exam: router

--- exam/src/App.tsx
+++ exam/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {HashRouter, Route, Routes} from 'react-router-dom';
+
 import './App.css'
+import {TranslatePage} from "./pages/translate/TranslatePage.tsx";
+import {HistoryPage} from "./pages/history/HistoryPage.tsx";
+import {NotFoundPage} from "./pages/notFound/NotFoundPage.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<TranslatePage />} />
+                <Route path='/history/' element={<HistoryPage />} />
+                <Route path='*' element={<NotFoundPage />}/>
+            </Routes>
+        </HashRouter>
+    </div>
+  );
 }
 
 export default App
