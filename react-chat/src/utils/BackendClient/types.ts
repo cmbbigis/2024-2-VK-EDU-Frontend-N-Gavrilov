e15f28befed6328893814947b65442cfa830e7b8
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-23 13:57:55 +0500

    hw14: рефакторинг и описания запросов

--- react-chat/src/utils/BackendClient/types.ts
+++ react-chat/src/utils/BackendClient/types.ts
@@ -1,3 +1,12 @@
+export interface IRegisterRequest {
+    username: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    bio?: string,
+    avatar?: File,
+}
+
 export interface IRegisterResponse {
     id: string,
     username: string,
@@ -9,14 +18,29 @@ export interface IRegisterResponse {
     last_online_at: string
 }
 
+export interface IAuthRequest {
+    username: string,
+    password: string,
+}
+
 export interface IAuthResponse {
     access: string,
+    refresh: string
+}
+
+export interface IRefreshRequest {
     refresh: string
 }
 
 export interface IRefreshResponse {
     access: string,
     refresh: string
+}
+
+export interface IGetChatsRequest {
+    page: number,
+    page_size: number,
+    search: string,
 }
 
 export interface IGetChatsResponse {
@@ -26,17 +50,28 @@ export interface IGetChatsResponse {
     results: IChat[]
 }
 
+export interface IGetChatRequest {
+    id: string,
+}
+
 export interface IGetChatResponse {
     id: string,
     title: string,
     members: IUser[],
     creator: IUser,
-    avatar: string | null,
+    avatar?: string,
     created_at: string,
     updated_at: string,
     is_private: boolean,
-    last_message: object | null,
+    last_message?: object,
     unread_messages_count: number
+}
+
+export interface ICreateChatRequest{
+    members: string[],
+    is_private: boolean,
+    title?: string,
+    avatar?: File
 }
 
 export interface ICreateChatResponse {
@@ -44,12 +79,19 @@ export interface ICreateChatResponse {
     title: string,
     members: IUser[],
     creator: IUser,
-    avatar: string | null,
+    avatar?: string,
     created_at: string,
     updated_at: string,
     is_private: boolean,
-    last_message: object | null,
+    last_message?: object,
     unread_messages_count: number,
+}
+
+export interface IGetMessagesRequest {
+    chat: string,
+    page: number,
+    page_size: number,
+    search: string,
 }
 
 export interface IGetMessagesResponse {
@@ -59,18 +101,27 @@ export interface IGetMessagesResponse {
     results: IMessage[]
 }
 
+export interface ICreateMessageRequest {
+    chat: string,
+    voice?: File,
+    text?: string,
+    files?: File[],
+}
+
 export interface ICreateMessageResponse {
     id: string,
-    text: string | null,
-    voice: string | null,
+    text?: string,
+    voice?: string,
     chat: string,
-    files: [
-        {
-            item: string,
-        }
-    ],
+    files?: IFile[],
     updated_at: string,
     created_at: string,
+}
+
+export interface IGetUsersRequest {
+    page: number,
+    page_size: number,
+    search: string,
 }
 
 export interface IGetUsersResponse {
@@ -80,15 +131,28 @@ export interface IGetUsersResponse {
     results: IUser[]
 }
 
+export interface IGetUserRequest {
+    id: string,
+}
+
 export interface IGetUserResponse {
     id: string,
     username: string,
     first_name: string,
     last_name: string,
-    bio: string | null,
-    avatar: string | null,
+    bio?: string,
+    avatar?: string,
     last_online_at: string,
     is_online: boolean,
+}
+
+export interface IEditProfileRequest {
+    id: string,
+    username?: string,
+    first_name?: string,
+    last_name?: string,
+    bio?: string,
+    avatar?: string,
 }
 
 export interface IEditProfileResponse {
@@ -96,16 +160,16 @@ export interface IEditProfileResponse {
     username: string,
     first_name: string,
     last_name: string,
-    bio: string | null,
-    avatar: string | null,
+    bio?: string,
+    avatar?: string,
     last_online_at: string,
     is_online: boolean,
 }
 
 export interface IOptions {
-    method?: string;
-    headers?: Record<string, string>;
-    body?: FormData;
+    method?: string,
+    headers?: Record<string, string>,
+    body?: FormData,
 }
 
 interface IUser {
@@ -113,8 +177,8 @@ interface IUser {
     username: string,
     first_name: string,
     last_name: string,
-    bio: string | null,
-    avatar: string | null,
+    bio?: string,
+    avatar?: string,
     last_online_at: string,
     is_online: boolean
 }
@@ -124,26 +188,26 @@ interface IChat {
     title: string,
     members: IUser[],
     creator: IUser,
-    avatar: string | null,
+    avatar?: string,
     created_at: string,
     updated_at: string,
     is_private: boolean,
-    last_message: object | null,
+    last_message?: object,
     unread_messages_count: number
 }
 
 interface IMessage {
     id: string,
-    text: string | null,
-    voice: string | null,
+    text?: string,
+    voice?: string,
     chat: string,
     sender: IUser,
-    files: [
-        {
-            item: string,
-        }
-    ],
+    files?: IFile[],
     was_read_by: IUser[],
     updated_at: string,
     created_at: string
+}
+
+interface IFile {
+    item: string,
 }
