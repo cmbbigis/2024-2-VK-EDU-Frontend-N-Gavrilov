757e12cccaaa11ade4026ba1d31f20e062312a72
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-23 23:16:28 +0500

    exam: небольшая доработка истории

--- exam/src/pages/history/HistoryPage.tsx
+++ exam/src/pages/history/HistoryPage.tsx
@@ -1,26 +1,33 @@
 import {Header} from "../../components/Header/Header.tsx";
 import {languages} from "../../languages.ts";
+import {useState} from "react";
 
 export const HistoryPage = () => {
     const languageMap = new Map<string, string>(Object.entries(languages));
+    const initializeHistory = (): string[] => {
+        const historyAsString: string = localStorage.getItem("history") || '';
+        if (!historyAsString.trim()) {
+            return [];
+        } else {
+            return JSON.parse(historyAsString);
+        }
+    };
 
-    const historyAsString: string = localStorage.getItem("history") || '';
-    let history: string[];
-    if (!historyAsString.trim()) {
-        history = [];
-    } else {
-        history = JSON.parse(historyAsString);
-    }
+    const [history, setHistory] = useState<string[]>(initializeHistory);
 
     return (
         <>
             <Header title="История" />
+            <button onClick={() => {
+                localStorage.removeItem('history');
+                setHistory([]);
+            }}>Очистить историю</button>
             <div id="chat-list" className="chat-list">
                 {
-                    history.map((historyItem) => {
+                    history.map((historyItem, index) => {
                         const historyItemArray = historyItem.split('/');
                         return (
-                            <div>
+                            <div key={index}>
                                 {`Текст: ${historyItemArray[0]} / Из языка: ${languageMap.get(historyItemArray[1])} / В язык: ${languageMap.get(historyItemArray[2])}`}
                             </div>
                         );
