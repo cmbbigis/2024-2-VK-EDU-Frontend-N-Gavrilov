800525f5b74b1617cdccac7eff595265defc0594
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-23 22:51:03 +0500

    exam: перевод с любого на любой

--- exam/src/components/TranslateWindow/TranslateWindow.tsx
+++ exam/src/components/TranslateWindow/TranslateWindow.tsx
@@ -3,13 +3,15 @@ import {useDispatch, useSelector} from "react-redux";
 
 import './TranslateWindow.css';
 import React from "react";
-import {setTextToTranslate, setTranslatedText} from "../../redux/slice.ts";
+import {setTextToTranslate, setTranslatedText, setTranslateFrom, setTranslateTo} from "../../redux/slice.ts";
 import {RootState} from "../../redux/RootState.ts";
 import * as TranslateUtils from "../../ts/utils/translate.ts";
+import {languages} from "../../languages.ts";
 
 export const TranslateWindow: React.FC = ()  => {
     const dispatch = useDispatch();
-    const { textToTranslate, translatedText } = useSelector((state: RootState) => state.slice)
+    const { translateTo, translateFrom, textToTranslate, translatedText } = useSelector((state: RootState) => state.slice)
+    const languageMap = new Map<string, string>(Object.entries(languages));
 
     const handleInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         dispatch(setTextToTranslate(event.target.value));
@@ -21,15 +23,53 @@ export const TranslateWindow: React.FC = ()  => {
         dispatch(setTranslatedText(response));
     };
 
+    const handleTranslateToChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        dispatch(setTranslateTo(event.target.value));
+    };
+
+    const handleTranslateFromChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        dispatch(setTranslateFrom(event.target.value));
+    };
 
     return (
         <div className="translate-window">
             <div className="translate-window-header">
-                <div className="translate-window-header-left">HeaderLeft</div>
+                <div className="translate-window-header-left">
+                    <span>{languageMap.get(translateFrom)}</span>
+                    <select
+                        className="interlocutors-select"
+                        name="members"
+                        onChange={handleTranslateFromChange}
+                    >
+                        {
+                            Object.entries(languages).map(([code, name]) => (
+                                <option key={name} value={code}>
+                                    {name}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
                 <span className="switch-button">
                     <SwapHorizIcon/>
                 </span>
-                <div className="translate-window-header-right">HeaderRight</div>
+                <div className="translate-window-header-right">
+                    <span>{languageMap.get(translateTo)}</span>
+                    <select
+                        className="interlocutors-select"
+                        name="members"
+                        onChange={handleTranslateToChange}
+                    >
+                        {
+                            Object.entries(languages).map(([code, name]) => (
+                                code !== 'Autodetect' && (
+                                <option key={code} value={code}>
+                                    {name}
+                                </option>
+                            )))
+                        }
+                    </select>
+                </div>
             </div>
             <div className="translate-window-body">
                 <div className="translate-window-body-left">
--- exam/src/redux/slice.ts
+++ exam/src/redux/slice.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     translateFrom: 'Autodetect',
-    translateTo: '',
+    translateTo: 'en-GB',
     textToTranslate: '',
     translatedText: '',
 };
